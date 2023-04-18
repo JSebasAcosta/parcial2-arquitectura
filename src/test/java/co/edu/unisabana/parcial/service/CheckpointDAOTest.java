7c1217d549c82202635a33185c9cdc4935e10557
JSebasAcosta <js09acosta@gmail.com>
2023-04-18 17:30:31 -0500

    Agrega application test

---
+++ src/test/java/co/edu/unisabana/parcial/service/CheckpointDAOTest.java
@@ -0,0 +1,24 @@
+package co.edu.unisabana.parcial.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckpointDAOTest {
+
+    @Test
+    void saveCheckin() {
+    }
+
+    @Test
+    void saveCheckout() {
+    }
+
+    @Test
+    void findLastCheckin() {
+    }
+
+    @Test
+    void finishCheckin() {
+    }
+}
---
+++ src/test/java/co/edu/unisabana/parcial/service/CheckpointServiceTest.java
@@ -0,0 +1,16 @@
+package co.edu.unisabana.parcial.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckpointServiceTest {
+
+    @Test
+    void checkin() {
+    }
+
+    @Test
+    void checkout() {
+    }
+}
